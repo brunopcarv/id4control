252f1814abf4d1e94448c4606ef339b515fdcaa9
Bruno Carvalho <brunopcarv@gmail.com>
2020-06-19 01:23:10 -0300

    Add plants and closed loop modules

---
+++ model_closedloop.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 vi:noet
+#  Closed-loop system - Plant and Controller
+
+import numpy as np
+
+class ClosedLoopSystem():
+
+	def __init__(self, plant, controller, xo, dt=0.01):
+		self.plant = plant
+		self.controller = controller
+		self.x = xo
+		self.xo = xo
+		self.k = 0
+		self.dt = dt # delta t: sample period
+
+	def next_points(self):
+		x_temp = self.x
+
+		self.x = self.plant.dynamics(x_temp, self.controller.action(x_temp), self.dt)
+		self.k += 1
+		return self.x
+
+	def	reset(self):
+		self.x = self.xo
+		self.k = 0
+
+	def run(self, k):
+		x_list = [self.x]
+
+		for i in range(1, k):
+			x_list = np.append(x_list, [self.x], axis=0)
+			self.next_points()
+
+		self.reset()
+		return x_list, range(k)
+
+
+if __name__ == "__main__":
+	from model_plants import LinearPlant, LinearDCMotorPlant, \
+	InvertedPendulum, PredatorPreyPlant
+
+	# Scalar zero controller 
+	class NoController():
+		def __init__(self):
+			pass
+		def action(self, x):
+			return 0
+
+	controller = NoController()
+	plant = PredatorPreyPlant(1.0, 1.0, 1.0, 1.0)
+
+	xo = np.array([1.0, 1.0]).T
+	closed_loop = ClosedLoopSystem(plant, controller, xo)
+
+	final_time_unit = 2000
+	half = int(final_time_unit*0.5)
+
+	x, time = closed_loop.run(final_time_unit)
+	x1 = x[:,0]
+	x2 = x[:,1]
+
+	X = np.array([x1[:half-1], x2[:half-1]]).T
+	Y = np.array([x1[1:half], x2[1:half]]).T
+
+	from id_kernel_ridge_regression import KernelRidgeRegression, \
+	kernel_rbf_function_M, kernel_rbf_function_N, kernel_linear_function, \
+	kernel_poly_function, kernel_tanh_function
+	from id_linear_ridge_regression import LinearRidgeRegression
+
+	# RBF kernel id
+	lambda_reg = 0.00001
+	regression = KernelRidgeRegression(lambda_reg)
+	regression.training(X, Y, kernel_rbf_function_M)
+
+	Y_ridge = np.array([x1[:half], x2[:half]]).T
+	for k in range(half,final_time_unit):
+		y, N = regression.predict(Y_ridge[-1,:], kernel_rbf_function_N)
+		Y_ridge = np.append(Y_ridge, [y], axis=0)
+
+	# Plot
+	import matplotlib.pyplot as plt
+	fig, axs = plt.subplots(2, 1)
+	axs[0].plot(time, x1, time, Y_ridge[:,0])
+	axs[0].set_xlim(0,final_time_unit)
+	axs[0].set_xlabel('Time units (k)')
+	axs[0].set_ylabel('Prey: x1 (actual) and x1 (ridge)')
+	axs[0].grid(True)
+
+	axs[1].plot(time, x2, time, Y_ridge[:,1])
+	axs[1].set_xlim(0,final_time_unit)
+	axs[1].set_xlabel('Time units (k)')
+	axs[1].set_ylabel('Pred: x2 (actual) and x2 (ridge)')
+	axs[1].grid(True)
+	# cxy, f = axs[1].cohere(x1, x2, 5, 1. / dt)
+
+	fig.tight_layout()
+	plt.show()
---
+++ model_plants.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 vi:noet
+#  Collection of system plants
+
+import numpy as np
+
+
+### General linear plant
+class LinearPlant():
+	def __init__(self, A, B, C):
+		self.A = A
+		self.B = B
+		self.C = C
+		self.D = D
+
+	def dynamics(self, x, u):
+		return self.A.dot(x) + self.B.dot(u)
+
+	def output(self, x, u):
+		return self.C.dot(x) + self.D.dot(u)
+
+
+### DC Motor - Linear
+class LinearDCMotorPlant():
+	"""
+	States:
+	w = load angular rate
+	i = current
+
+	Input:
+	v_app = applied voltage
+
+	Output (measured quantity):
+	w = load angular rate
+
+	Model parameters:
+	J = inertial load
+	Kb = emf constant
+	Kf = linear viscous friction constant
+	Km = armature constant
+	R = circuit resitance
+	L = armature self-inductance
+
+	References:
+	- https://ww2.mathworks.cn/help/control/getstart/linear-lti-models.html
+	- http://webfiles.portal.chalmers.se/et/MSc/BaldurssonStefanMSc.pdf
+	"""
+
+	def __init__(self, J, Kb, Kf, Km, R, L):
+		self.A = np.array([[-R/L, -Kb/L],[Km/J, -Kf/J]])
+		self.B = np.array([1/L, 0]).T
+		self.C = np.array([0, 1])
+
+	def dynamics(self, x, u, dt):
+		return x + (self.A.dot(x) + self.B.dot(u))*dt
+
+	def output(self, x, u):
+		return self.C.dot(x)
+
+
+### Predator Prey - Nonlinear
+class PredatorPreyPlant():
+	"""
+	Parameters (growth, death, and mutual influences rates)
+	a, b, c, d = 1.0, 1.0, 1.0, 1.0
+
+	States
+	x1 = prey population
+	x2 = predator population
+
+	Dynamics
+	x1(k+1) = a x1(k) - b x1(k) x2(k) + u**2
+	x2(k+1) = - c x2(k) + d x1(k) x2(k)
+	"""
+	def __init__(self, a, b, c, d):
+		self.a = a
+		self.b = b
+		self.c = c
+		self.d = d
+
+	def dynamics(self, x, u, dt):
+		x0 = x[0]+ (self.a*x[0] - self.b*x[0]*x[1] + u**2)*dt
+		x1 = x[1]+ (-self.c*x[1] + self.d*x[0]*x[1])*dt
+		return np.array([x0, x1]).T
+
+	def output(self, x, u):
+		return x
+
+
+### Inverted Pnedulum - Nonlinear
+class InvertedPendulum():
+	"""
+	State space: x.T = [y ydot theta thetadot]
+	- y = cart position coordinate
+	- theta = pendulum angle from vertical
+
+	Parameters:
+	- M = cart mass
+	- m = pedulum mass
+	- b = friction coefficient
+	- I = mass moment of inertia of pendulum
+	- l = length to pendulum center of mass
+
+	Input:
+	u = [F]: force applied to cart
+
+	Reference: shorturl.at/cvGQ4
+	"""
+
+	GRATVITY = 9.807
+
+	def __init__(self, M=0.5, m=0.2, b=0.1, I=0.006, l=0.3):
+		self.M = M
+		self.m = m
+		self.b = b
+		self.I = I
+		self.l = l
+		self.dt = dt
+
+	def dynamics(self, x, u, dt):
+		den = self.I + self.m*self.l**2
+		bigden = self.M + self.m - (self.m*self.l*np.cos(x[2]))**2/den
+		num0 = -self.b*x[1]
+		num1 = (self.m*self.l)**2*GRAVITY*np.sin(x[2])*np.cos(x[2])/den
+		num2 = self.m*self.l*x[3]**2*np.sin(x[2])
+		x0 = x[0] + x[1]*dt 
+		x1 = x[1] + (num0+num1+num2+u)/bigden*dt
+		x2 = x[2] + x[3]*dt
+		x3 = x[3] + (-self.m*self.l*(x1*np.cos(x[2])+GRAVITY*np.sin(x[2])))/den*dt
+		return np.array([x0, x1, x2, x3]).T
+
+	def output(self, x, u):
+		return x
